90675b194b25f797bb6a5e2d389341286a879e08
Clement Poh <clement@poh.cc>
2024-06-07 19:38:57 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # rust-cons-list
-Implementation of the functional structure the cons list in Rust
+
+Implementation of the data structure the cons list in Rust. It is essentially a
+linked list defined recursively and popular in functional programming languages
+like Lisp and Haskell. Chapter 15 of the [Rust
+Book](https://doc.rust-lang.org/book/ch15-01-box.html) introduces the concept
+and the basic implementation of the list in Rust.
+
+While the linked list is disfavored in Rust, it's a familiar structure to many
+functional programmers.
+
+This library extends the implementation of the basic cons list found in the Rust
+Book to offer conveniences similar to those implemented for the vector.
+
+## Usage
+
+### Definition
+
+The type definition is lifted straight out of the Book
+
+``` rust
+#[derive(Debug, Default, PartialEq, PartialOrd)]
+enum List<T> {
+    #[default]
+    Nil,
+    Cons(T, Box<List<T>>),
+}
+```
+
+### Creating a new list
+
+A macro is defined to allow for easy instantiation.
+
+``` rust
+  let list = cons![1, 2, 3]
+  assert_eq!(list, Cons(1, Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+```
---
+++ src/lib.rs
@@ -0,0 +1,209 @@
+//! # Rust Cons List
+//!
+//! Implementation of the functional structure the cons list in Rust.
+use crate::List::{Cons, Nil};
+
+#[derive(Debug, Default, PartialEq, PartialOrd)]
+enum List<T> {
+    #[default]
+    Nil,
+    Cons(T, Box<List<T>>),
+}
+
+#[macro_export]
+macro_rules! cons {
+    // Empty list
+    [] => {
+        Nil
+    };
+
+    // Singleton list
+    [ $head:expr ] => {
+        Cons($head, Box::new(Nil))
+    };
+
+    // Recursive case: more than one item
+    [$head:expr, $($tail:expr),+] => {
+        Cons($head, Box::new( cons!($($tail), +) ))
+    };
+ }
+
+#[macro_export]
+macro_rules! cons_rev {
+[ $( $x:expr ), *] => {
+        {
+            let list = Nil;
+            $(
+                let list = Cons($x, Box::new(list));
+            )*
+            list
+        }
+    };
+}
+
+// Define the iterator struct for List
+struct ListIntoIterator<T> {
+    list: List<T>,
+}
+
+impl<T> IntoIterator for List<T> {
+    type Item = T;
+    type IntoIter = ListIntoIterator<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ListIntoIterator { list: self }
+    }
+}
+
+impl<T> Iterator for ListIntoIterator<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Take ownership of self.list and replace it with the default Nil
+        // value. This allows us to work with the original list value without
+        // violating borrowing rules.
+        let list = std::mem::take(&mut self.list);
+        match list {
+            Cons(head, tail) => {
+                self.list = *tail; // Move ownership of the tail back to self.list
+                Some(head)
+            }
+            Nil => None,
+        }
+    }
+}
+
+impl<'a, T> Iterator for &'a List<T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            Nil => None,
+            Cons(x, xs) => {
+                // replaces the contents of the reference self (which initially
+                // points to the whole list) with xs (the rest of the list).
+                // This "moves" the reference down the list.
+                let _ = std::mem::replace(self, xs);
+                Some(x)
+            }
+        }
+    }
+}
+
+impl<T> FromIterator<T> for List<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut list: List<T> = List::new();
+
+        for x in iter {
+            list = Cons(x, Box::new(list))
+        }
+
+        // FIXME: currently returns a reversed list
+        list
+    }
+}
+
+impl<T> List<T> {
+    fn new() -> List<T> {
+        Nil
+    }
+
+    fn push(x: T, xs: List<T>) -> List<T> {
+        Cons(x, Box::new(xs))
+    }
+
+    fn len(&self) -> usize {
+        self.count()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::List;
+    use crate::List::Cons;
+    use crate::List::Nil;
+
+    #[test]
+    fn construction() {
+        let list = Cons(2, Box::new(Nil));
+
+        println!("{:?}", list);
+
+        assert_eq!(Cons(2, Box::new(Nil)), list);
+
+        let list = Cons(1, Box::new(list));
+        assert_eq!(Cons(1, Box::new(Cons(2, Box::new(Nil)))), list);
+    }
+
+    #[test]
+    fn macro_empty() {
+        let empty: List<i32> = Nil;
+
+        assert_eq!(empty, cons![]);
+    }
+
+    #[test]
+    fn macro_singleton() {
+        let singleton: List<i32> = Cons(1, Box::new(Nil));
+
+        assert_eq!(singleton, cons![1]);
+    }
+
+    #[test]
+    fn macro_test() {
+        let xs = Cons(2, Box::new(Cons(3, Box::new(Nil))));
+        assert_eq!(cons![2, 3], xs);
+
+        let xs = Cons(1, Box::new(xs));
+        assert_eq!(cons![1, 2, 3], xs);
+    }
+
+    #[test]
+    fn iter_copyable_test() {
+        let xs = cons![1, 2, 3];
+        let ys: List<i32> = xs.map(|x| x * 2).collect();
+
+        println!("{:?}", xs);
+        println!("{:?}", ys);
+
+        assert_eq!(cons![2, 4, 6], ys)
+    }
+
+    #[test]
+    fn iter_test() {
+        let xs = cons![Some(1), Some(2), Some(3)];
+
+        for x in xs.enumerate() {
+            println!("{:?}", x);
+        }
+
+        for x in &xs {
+            println!("{:?}", x);
+        }
+
+        assert_eq!(xs, cons![Some(1), Some(2), Some(3)])
+    }
+
+    #[test]
+    fn into_iter_test() {
+        let xs = cons![1, 2, 3];
+        let vector: Vec<i32> = xs.into_iter().collect();
+
+        assert_eq!(vec![1, 2, 3], vector);
+    }
+
+    #[test]
+    fn len_test() {
+        let xs: List<i32> = cons![];
+        assert_eq!(0, xs.len());
+
+        let xs = cons![2];
+        assert_eq!(1, xs.len());
+
+        let xs = cons![1, 2, 3, 4];
+        assert_eq!(4, xs.len());
+
+        let xs = cons![Some(1), Some(2), Some(3)];
+        assert_eq!(3, xs.len());
+    }
+}
